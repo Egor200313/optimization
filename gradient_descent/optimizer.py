7c537b5e667fae22daa8c2e6ee6a71f4c05e999e
kuznetsov nikita <kuznetsov.ne@phystech.edu>
2022-11-20 10:51:48 +0300

    Added notebook, solved first task

--- gradient_descent/optimizer.py
+++ gradient_descent/optimizer.py
@@ -2,7 +2,6 @@ from one_dim.optimizer import optimize_one_dim
 from typing import Callable
 import numpy as np
 
-
 def optimize_hs(tolerance: float,
                 one_dim_tolerance: float,
                 max_iter: int,
@@ -10,7 +9,7 @@ def optimize_hs(tolerance: float,
                 target_function: Callable,
                 derivative: Callable = None
                 ) -> dict:
-	x_cur = np.ones(dim)
+	x_cur = np.zeros(dim)
 	d_cur = -derivative(x_cur)
 	for _ in np.arange(1, max_iter):
 		alpha = optimize_one_dim(-100, 100, one_dim_tolerance, 10000,
@@ -31,67 +30,75 @@ def optimize_hs(tolerance: float,
 def gradient_descent(target_function: Callable,
 		     derivative: Callable,
 		     dim: int,
+		     x0,
 		     tolerance: float,
+		     one_dim_tolerance: float,
 		     max_iter: int,
-		     termination_criterion = "cauchy") -> dict:
-    x_cur = np.ones(dim)
-    d_cur = -derivative(x_cur)
+		     termination_criterion: str) -> dict:
+	x_cur = x0
+	d_cur = -derivative(x_cur)
     
-    i = 0
-    while i < max_iter:
-    	x_next = x_cur + d_cur
-    	d_next = -derivative(x_next)
+	i = 0
+	while i < max_iter:
+		alpha = optimize_one_dim(-100, 100, one_dim_tolerance, 10000,
+                                         lambda x: target_function(x_cur + x * d_cur))['point']
+		x_next = x_cur + alpha * d_cur
+		d_next = -derivative(x_next)
     	
-    	if termination_criterion == "cauchy":
-    	    if abs(target_function(x_cur) - target_function(x_next)) < tolerance:
-    	    	x_cur = x_next
-    	    	break
-    	elif termination_criterion == "cauchy_x":
-    	    if np.sqrt((x_cur - x_next) @ (x_cur - x_next).T) < tolerance:
-    	        x_cur = x_next
-    	        break
-    	else: # termination_criterion == "zero_gradient"
-    	    if np.sqrt(derivative(x_next).T @ derivative(x_next)) < tolerance:
-    	    	x_cur = x_next
-    	    	break
+		if termination_criterion == "cauchy":
+			if abs(target_function(x_cur) - target_function(x_next)) < tolerance:
+				return {'point': x_next}
+		elif termination_criterion == "cauchy_x":
+			if np.sqrt((x_cur - x_next) @ (x_cur - x_next).T) < tolerance:
+				return {'point': x_next}
+		elif termination_criterion == "zero_gradient":
+			if np.sqrt(derivative(x_next).T @ derivative(x_next)) < tolerance:
+				return {'point': x_next}
+		else:
+			if abs(d_cur.T @ (derivative(x_next) - derivative(x_cur))) < tolerance:
+				return {'point': x_next}
     	
-    	x_cur, d_cur = x_next, d_next
-    	i += 1
+		x_cur, d_cur = x_next, d_next
+		i += 1
     	
-    return {'point': x_cur}
+	return {'point': x_cur}
     
-def hestens_stiefel(target_function: Callable,
+def hestenes_stiefel(target_function: Callable,
 		    derivative: Callable,
 		    dim: int,
+		    x0,
 		    tolerance: float,
+		    one_dim_tolerance: float,
 		    max_iter: int,
-		    termination_criterion = "cauchy") -> dict:
-    x_cur = np.ones(dim)
-    d_cur = -derivative(x_cur)
+		    termination_criterion: str) -> dict:
+	x_cur = x0
+	d_cur = -derivative(x_cur)
     
-    i = 0
-    while i < max_iter:
-    	x_next = x_cur + d_cur
+	i = 0
+	while i < max_iter:
+		alpha = optimize_one_dim(-100, 100, one_dim_tolerance, 10000,
+                                         lambda x: target_function(x_cur + x * d_cur))['point']
+		x_next = x_cur + alpha * d_cur
     	
-    	coef = (derivative(x_next).T @ (derivative(x_next) - derivative(x_cur)))\
-    	       / (d_cur @ (derivative(x_next) - derivative(x_cur)).T)
-    	d_next = -derivative(x_next) + coef * d_cur
+		coef = (derivative(x_next).T @ (derivative(x_next) - derivative(x_cur)))\
+    	             / (d_cur.T @ (derivative(x_next) - derivative(x_cur)))
+		d_next = -derivative(x_next) + coef * d_cur
     	
-    	if termination_criterion == "cauchy":
-    	    if abs(target_function(x_cur) - target_function(x_next)) < tolerance:
-    	    	x_cur = x_next
-    	    	break
-    	elif termination_criterion == "cauchy_x":
-    	    if np.sqrt((x_cur - x_next) @ (x_cur - x_next).T) < tolerance:
-    	        x_cur = x_next
-    	        break
-    	else: # termination_criterion == "zero_gradient"
-    	    if np.sqrt(derivative(x_next).T @ derivative(x_next)) < tolerance:
-    	    	x_cur = x_next
-    	    	break
+		if termination_criterion == "cauchy":
+			if abs(target_function(x_cur) - target_function(x_next)) < tolerance:
+				return {'point': x_next}
+		elif termination_criterion == "cauchy_x":
+			if np.sqrt((x_cur - x_next) @ (x_cur - x_next).T) < tolerance:
+				return {'point': x_next}
+		elif termination_criterion == "zero_gradient":
+			if np.sqrt(derivative(x_next).T @ derivative(x_next)) < tolerance:
+				return {'point': x_next}
+		else:
+			if abs(d_cur.T @ (derivative(x_next) - derivative(x_cur))) < tolerance:
+				return {'point': x_next}
     	
-    	x_cur, d_cur = x_next, d_next
-    	i += 1
+		x_cur, d_cur = x_next, d_next
+		i += 1
     	
-    return {'point': x_cur}
+	return {'point': x_cur}
 
